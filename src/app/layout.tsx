5aaaac04c2166d9a111fdafe3122b9e4f12c2293
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-02-13 19:30:46 +0500

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { GeistSans } from 'geist/font/sans';
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+
+
+
+export const metadata: Metadata = {
+  title: "Next js app",
+  description: "Next js app",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={GeistSans.className}>
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
+
