7be85f2c7aadf04147b775a69ffaca499372f9b6
Haseeb Ahmed Raza Khan <web.dev.haseeb@gmail.com>
2025-02-16 08:13:41 +0500

    fixing build error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,15 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
-
-export default eslintConfig;
+export default {
+  "extends": "next/core-web-vitals",
+  "rules": {
+    // "react/no-unescaped-entities": "off",
+    // "react/jsx-key": "off",
+    // "@next/next/no-img-element": "off",
+    // "react-hooks/exhaustive-deps": "off",
+    // "react/display-name": "off",
+    // "@next/next/no-page-custom-font": "off",
+    // "react/no-unknown-property": "off",
+    // "@next/next/no-html-link-for-pages": "off",
+    // "jsx-a11y/alt-text": "off",
+    // "react/jsx-no-target-blank": "off"
+  }
+};
